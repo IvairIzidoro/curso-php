c507b43a6dbb4fd560fd391db6033bdeb1550cbc
Ivair Izidoro <ivair.izidoro@gmail.com>
2016-09-28 20:17:38 -0300

    Algumas correções no html

--- aula3/editar.php
+++ aula3/editar.php
@@ -10,17 +10,25 @@ $result = mysql_fetch_array($query);
  ?>
 
 <?php include_once('include-header.php');?>
+
 	<h1>Tarefa: <?= $result['tarefa']?></h1>
-        <form class="formEditarTarefa" action="updateTarefa.php" method="post">
-	        <div class="form-group">	
-                <label for="tarefa" class="col-sm3 control-label">Tarefa</label>
-	            <input type="hidden" name="id" value="<?= $result['id']?>" >
-		    </div>
-		    <div class="form-group col-sm-6">
-		        <input type="text" name="tarefa" id="tarefa" value="<?= $result['tarefa']?> " class="form-control">
-		    </div>
-		    <div class="form-group">
-		        <input type="submit" name="enviar" value="Enviar" class="col-sm-2 btn btn-success">
-            </div>
-		</form>
+       <div class="panel panel-default"> 
+            <form class="formEditarTarefa form-horizontal" action="updateTarefa.php" method="post">
+               
+                <div class="form-group">	
+                    <label for="tarefa" class="col-sm-3 control-label">Tarefa: </label>
+                    <input type="hidden" name="id" value="<?= $result['id']?>" >
+                   
+                    <div class="col-sm-6">
+                        <input type="text" name="tarefa" id="tarefa" value="<?= $result['tarefa']?> " class="form-control">
+                    </div>
+                </div>
+                               
+                <div class="form-group">
+                    <div class="col-sm-offset-3 col-sm-12">
+                        <input type="submit" name="enviar" value="Enviar" class="btn btn-default">
+                    </div>
+                </div>
+            </form>
+		</div>
 <?php include_once('include-footer.php');?>	
